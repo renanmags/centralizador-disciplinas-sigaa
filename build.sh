a40b135474c58f713a12fab991c52c5815ad606d
renanmags <renandmagalhaes877@gmail.com>
2025-07-21 20:45:12 -0300

    versao de deploy com build alterado

--- build.sh
+++ build.sh
@@ -2,13 +2,21 @@
 # exit on error
 set -o errexit
 
+echo "Iniciando o script de build..."
+
 # Instala as dependências do Python
 pip install -r requirements.txt
 
-# Comandos para instalar o Google Chrome
-echo "Iniciando a instalação do Google Chrome..."
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-apt-get update
-apt-get install -y google-chrome-stable
-echo "Google Chrome instalado com sucesso."
+# Instala o Google Chrome de forma manual, sem precisar de 'sudo'
+echo "Instalando o Google Chrome..."
+wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x /tmp/google-chrome-stable_current_amd64.deb /tmp/chrome
+rm /tmp/google-chrome-stable_current_amd64.deb
+
+# Move os arquivos do Chrome para uma pasta que persiste após o build
+# O Selenium Manager irá encontrar o executável do Chrome aqui.
+mv /tmp/chrome/opt/google/chrome /opt/render/project/src/chrome
+# Adiciona a pasta do Chrome ao PATH do sistema
+export PATH="${PATH}:/opt/render/project/src/chrome"
+
+echo "Build concluído com sucesso."
